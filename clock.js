acc394d417e08fe257b70d08913a9c9f4639e0de
Bryan Brinson <bryan@beb.com>
2019-07-29 17:50:20 -0700

    function second hand movement

--- clock.js
+++ clock.js
@@ -1,30 +1,59 @@
-
 // set 3 intervals? hh, mm, ss. set approp milliseconds for hand:
-let sec = 1000; 
+let sec = 1000;
 let min = sec * 60;
 let hour = min * 60;
-// let now = new Date();
+let now = new Date();
+console.log(sec, min, hour);
 
 let secInterval, minInterval, hourInterval;
 
-document.addEventListener('DOMContentLoaded', function(){
-    // init the page
+document.addEventListener("DOMContentLoaded", function() {
+  // init the page
+  secInterval = setInterval(secMgr, sec);
+  minInterval = setInterval(minMgr, min);
+  // hourInterval = setInterval(hourMgr, hour);
 });
 
-secInterval = setInterval(secMgr, sec);
-minInterval = setInterval(minMgr, min);
-hourInterval = setInterval(hourMgr, hour);
+let minMgr = function() {
+  let now = new Date();
+  let minDegree = minRotation(now.getMinutes());
+  updateHand("minute-hand", minDegree);
+};
+
+let minRotation = function(curMin) {
+  if (curMin === 60) {
+    return 0;
+  } else {
+    return (curMin / 60) * 60;
+  }
+};
 
 let secMgr = function() {
-    
+  let now = new Date();
+  let secDegree = secondRotation(now.getSeconds()); // returns (59 / 60) * 360
+  updateHand("second-hand", secDegree);
 };
 
-let minMgr = function() {
-
+let secondRotation = function(curSec) {
+  // (curSec === 0) ?  0 : (curSec / 60) * 360;
+  if (curSec === 0) {
+    return 0;
+  } else {
+    return (curSec / 60) * 360;
+  }
 };
 
-let hourMgr = function() {
-
+let updateHand = function(hand, degree) {
+  document.getElementById(`${hand}`).style.transform = `rotate(${degree}deg)`;
 };
 
+// let minMgr = function() {
 
+// };
+
+// let hourMgr = function() {
+
+// hourRotation(12); // returns 0
+// hourRotation(5); // returns (5 / 12) * 360
+
+// };
